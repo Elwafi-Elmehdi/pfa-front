023726c03bf9d2f13f755ec86b9c5a87f0255eee
Mehdi <elwafi.courrier99@gmail.com>
2022-06-26 20:12:14 +0100

    Added model

--- src/app/pages/services/device.service.ts
+++ src/app/pages/services/device.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Device } from '../models/device';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeviceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private readonly url: string = `${environment.url}/devices`;
+  private device: Device | null;
+  private devices: Device[] | null;
+
+
 }
