eb5c540bcaf7c777260eee479c5796ca48dea892
Mehdi <elwafi.courrier99@gmail.com>
2022-07-01 16:45:34 +0100

    Added All method for consuming device resource

--- src/app/pages/services/device.service.ts
+++ src/app/pages/services/device.service.ts
@@ -14,5 +14,37 @@ export class DeviceService {
   private device: Device | null;
   private devices: Device[] | null;
 
+  public loadAll() {
+    this.http.get<Device[]>(this.url + '/').subscribe(res => {
+      this.devices = res;
+    });
+  }
+
+  public getDeviceById(id: number) {
+    this.http.get<Device>(this.url + `/${id}/`).subscribe(res => {
+      this.devices = [res];
+    })
+  }
+  public saveDevice(device: Device) {
+    if (device !== null) {
+      this.http.post<Device>(this.url + '/', device).subscribe(res => {
+        this.loadAll();
+      })
+    }
+  }
+  public deleteDeviceById(id: number) {
+    if (id !== null && id <= 0) {
+      this.http.delete(this.url + `/${id}/`).subscribe(res => {
+        this.loadAll();
+      })
+    }
+  }
+  public updateDeviceById(id: number, device: Device) {
+    if (id !== null && id <= 0 && device !== null) {
+      this.http.put(this.url + `/${id}/`, device).subscribe(res => {
+        this.loadAll();
+      })
+    }
+  }
 
 }
